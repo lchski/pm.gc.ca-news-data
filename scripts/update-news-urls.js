032deefe2521e5bd401ed6c99c7b8a5a135d35b7
Lucas Cherkewski <lucas@lucascherkewski.com>
2022-03-06 15:09:55 -0500

    Slight tweaks to broaden scope from news releases to all entries
    - Change AJAX payload (from view_args=1 to view_args=, which gets all
      entries)
    - Remove unnecessary comments
    - Reword some comments
    - Create urls.json and update script to point to it
    - Rename downloader

--- scripts/update-news-urls.js
+++ scripts/update-news-urls.js
@@ -4,17 +4,17 @@ import fs from 'fs';
 
 /*
 
-Scrape _all_ news release URLs:
+Scrape _all_ news entries URLs:
 
 1. Load existing URLs from disk.
-2. Starting with N=0, load page N of pm.gc.ca news releases. Extract and store the last page number. If N = last page number, quit and save the URLs.
+2. Starting with N=0, load page N of pm.gc.ca news entries. Extract and store the last page number. If N = last page number, quit and save the URLs.
 3. Extract the URLs. Compare URLs scraped to URLs from disk. If they all match (i.e., all the scraped URLs are already saved), stop.
 4. Save expanded list of URLs, deduplicated, to disk.
-5. Go to step 2, using page n+1 of pm.gc.ca news releases.
+5. Go to step 2, using page n+1 of pm.gc.ca news entries.
 
 */
 
-const savedUrlsFilePath = 'data/source/urls-news-releases.json';
+const savedUrlsFilePath = 'urls.json';
 
 let lastPageNumber = 1; // We update this in the loop.
 
@@ -56,17 +56,9 @@ async function fetchNewsReleaseHtmlJSON(page) {
             "accept": "*/*",
             "accept-language": "en-US,en;q=0.9",
             "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-            // "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"98\", \"Google Chrome\";v=\"98\"",
-            // "sec-ch-ua-mobile": "?0",
-            // "sec-ch-ua-platform": "\"macOS\"",
-            // "sec-fetch-dest": "empty",
-            // "sec-fetch-mode": "cors",
-            // "sec-fetch-site": "same-origin",
             "x-requested-with": "XMLHttpRequest",
-            // "Referer": "https://pm.gc.ca/en/news/releases?page=1",
-            // "Referrer-Policy": "strict-origin-when-cross-origin"
         },
-        "body": `view_name=news&view_display_id=page_1&view_args=1&page=${page}`,
+        "body": `view_name=news&view_display_id=page_1&view_args=&page=${page}`,
         "method": "POST"
     });
     const responseJSON = await response.json();
@@ -98,10 +90,6 @@ function extractUrlsFromNewsPage(newsPageHtmlJSON) {
             .toArray()
             .map((linkElement) => newsReleaseListingHtml(linkElement).attr('href'))
             .filter((link) => link.startsWith('/en/news/')))];
-    
-    // To pull just "chnage in the ranks" URLs (which always include "change" and "rank")
-    // .filter((link) => link.includes('change') && link.includes('rank')))
-    // NB! Some include "reappointment"
 
     return scrapedUrls;
 }
